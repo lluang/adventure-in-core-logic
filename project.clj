665ed835ba1451eaaf14e4cb9dcc908e81efba20
Louis Luangkesorn <louis.luangkesorn@gmail.com>
2025-07-12 14:21:07 -0400

    updated project.clj for core.logic 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adventure-in-core-logic "2.0.0-SNAPSHOT"
-  :description "Adventure in Clojure core.logic, update to 1.1.0"
+(defproject adventure-in-core-logic "1.0.0-SNAPSHOT"
+  :description "Adventure in Clojure core.logic"
   :dependencies [[org.clojure/clojure "1.11.0"]
-                [org.clojure/core.logic "1.1.0"]])
+                 [org.clojure/core.logic "1.1.0"]])
